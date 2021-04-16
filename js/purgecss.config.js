473ed5ea40381f35c1d7f55c3d1ed8c1ff5f0eb7
Hilali34 <abdelali034@gmail.com>
2021-04-16 13:38:53 +0200

    optimisation fichier css purification momification et combinaison

---
+++ js/purgecss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: ['index.html','contact.html'],
+    css: ['style.css','./css/bootstrap.css', './css/et-line.css', './css/font-awesome.css'],
+    keyframes:true,
+    rejected:true,
+    variables:true,
+    output:'./css-purified'
+
+  }
